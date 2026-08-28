00eae23133a7132c3d6b29da75d78b28c59266db
0x....apus <folorunsopraise12@gmail.com>
2026-08-28 23:59:41 +0100

    Running test 1 staging

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -12,5 +12,17 @@ module.exports = {
       time: true,
       env: { NODE_ENV: 'production', PORT: 3300 },
     },
+    {
+      name: 'pepsa-admin-workers',
+      script: 'dist/worker.js',
+      exec_mode: 'fork',
+      instances: 1,
+      max_memory_restart: '512M',
+      kill_timeout: 15000,
+      autorestart: true,
+      merge_logs: true,
+      time: true,
+      env: { NODE_ENV: 'production' },
+    },
   ],
 };
